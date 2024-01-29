61b2af9bb3bae89b10c79853fcadf69b74848057
NoelRamosCamacho <157948494+NoelRamosCamacho@users.noreply.github.com>
2024-01-28 21:37:29 -0800

    Add files via upload

---
+++ op_comparacion.py
@@ -0,0 +1,39 @@
+#OPERADORES DE COMPARACIÓN
+print('Operadores de comparación')
+#Operador      Comparacion
+# ==           es igual que
+# !=           es distinto de
+# >            es mayor que
+# <            es menor que
+# >=           es mayor o igual que
+# <=           es menor o igual que
+
+igual_que = 5 == 6
+
+distinto_de = 5 != 6
+
+mayor_que = 5 > 6
+
+menor_que = 5 < 6
+
+mayor_o_igual = 5 >= 6
+
+menor_o_igual = 5 <= 6
+
+print(menor_o_igual)
+print()
+
+#Cálculos combinados
+print('''Cálculos combinados''')
+a = 5
+b = 10
+c = 20
+comparacion = a + b < c
+print(comparacion)
+print()
+
+#Comparacion de usuarios
+print("Comparación de usuarios")
+contraseña_almacenada = "CapitanRamos"
+contraseña_escrita = 'CapitanRamos'
+print(contraseña_almacenada == contraseña_escrita)
